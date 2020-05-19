1967b5becfb1a2c72065ee7b3a3fe0d3107f69dc
Brandilyar <63740901+Brandilyar@users.noreply.github.com>
2020-05-19 14:50:20 +0300

    Create Banch.py

---
+++ Banch/Banch.py
@@ -0,0 +1,24 @@
+count = 0
+def degree_recursion(degree,result_list=[]):
+    global count
+    count = count + 1
+    number = 5
+    if degree == 0 or degree == 1:
+        return 1
+    else:
+        result = number * degree_recursion(degree-1,result_list)
+        result_list.append(result)
+        if degree == count:
+            return result_list
+        else:
+            return result
+
+def degree_cycle(degree):
+    result = []
+    number = 5
+    if degree == 0 or degree == 1:
+        return 1 
+    else:
+        for i in range(1,degree):
+            result.append(number**i)
+    return result
